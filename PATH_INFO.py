9059c9a1cc566b587276e1c029fb880842b04cc6
Kpreetham <kumarpreetham@outlook.com>
2016-01-26 09:56:10 +0530

    Added dataset directories.

--- PATH_INFO.py
+++ PATH_INFO.py
@@ -3,3 +3,10 @@
 # of the dataset.
 
 DATASET_DIR = "/home/preetham/Project/Dataset/Dataset/"
+
+DATASET_CORRECTED = DATASET_DIR + 'corrected'
+DATASET_KDDCUP_DATA_CORRECTED = DATASET_DIR + 'kddcup.data.corrected'
+DATASET_KDDCUP_DATA_10_PERCENT_CORRECTED = DATASET_DIR + 'kddcup.data_10_percent_corrected'
+DATASET_KDDCUP_NEW_TEST_DATA_10_PERCENT_UNLABELED = DATASET_DIR + 'kddcup.newtestdata_10_percent_unlabeled'
+DATASET_KDDCUP_TESTDATA_UNLABELED = DATASET_DIR + 'kddcup.testdata.unlabeled'
+DATASET_KDDCUP_TESTDATA_UNLABELED_10_PERCENT = DATASET_DIR + 'kddcup.testdata.unlabeled_10_percent'
